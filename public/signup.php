3a7e1ad5d377b15f891530cd586c064fe5bef0fe
omar571mamdouh <ok1119076@gmail.com>
2025-02-05 23:49:46 +0200

    i added my backend validation for signup page

--- public/signup.php
+++ public/signup.php
@@ -1,6 +1,13 @@
+<?php
+
+require_once "../includes/valid_signup.php";
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +21,7 @@
             align-items: center;
             height: 100vh;
         }
+
         .container {
             display: flex;
             width: 800px;
@@ -22,26 +30,47 @@
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             overflow: hidden;
         }
+
         .form-container {
             width: 50%;
             padding: 10px;
         }
+
         .image-container {
             width: 50%;
             border-radius: 10px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             background: url('images/fryco.jpg') center/cover;
         }
+
         .btn-primary {
             background-color: rgb(159, 39, 39);
             border-color: rgb(159, 39, 39);
         }
+
         .btn-primary:hover {
             background-color: darkred;
             border-color: darkred;
         }
+
+        .error {
+            font-weight: bold;
+            color: red;
+            margin-top: 5px;
+        }
+        .error {
+    font-weight: bold;
+    color: red;
+    margin-top: 5px;
+    margin-bottom: 15px; 
+}
+
+.text-center{
+     margin-top: 300px;
+}
     </style>
 </head>
+
 <body>
     <div class="container">
         <div class="form-container">
@@ -49,36 +78,52 @@
             <form action="" method="POST" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label class="form-label">Name</label>
-                    <input type="text" name="user_name" class="form-control" required>
+                    <input type="text" name="user_name" class="form-control" value="<?php echo isset($name) ? $name : ''; ?>">
+                    <?php if (isset($errors['name'])) { ?>
+                        <p class="error"><?php echo $errors['name']; ?></p>
+                    <?php } ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" required>
+                    <input type="email" name="email" class="form-control" value="<?php echo isset($email) ? $email : ''; ?>">
+                    <?php if (isset($errors['email'])) { ?>
+                        <p class="error"><?php echo $errors['email']; ?></p>
+                    <?php } ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Password</label>
-                    <input type="password" name="passwordd" class="form-control" required>
+                    <input type="password" name="passwordd" class="form-control" value="<?php echo isset($password) ? $password : ''; ?>">
+                    <?php if (isset($errors['password'])) { ?>
+                        <p class="error"><?php echo $errors['password']; ?></p>
+                    <?php } ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Phone</label>
-                    <input type="number" name="phone" class="form-control" required>
+                    <input type="tel" name="phone" class="form-control" value="<?php echo isset($phone) ? $phone : ''; ?>">
+                    <?php if (isset($errors['phone'])) { ?>
+                        <p class="error"><?php echo $errors['phone']; ?></p>
+                    <?php } ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Profile Picture</label>
                     <input type="file" name="profile_picture" class="form-control">
+                    <?php if (isset($errors['file'])) { ?>
+                        <p class="error"><?php echo $errors['file']; ?></p>
+                    <?php } ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Address</label>
-                    <input type="text" name="addresss" class="form-control">
+                    <input type="text" name="addresss" class="form-control" value="<?php echo isset($address) ? $address : ''; ?>">
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" required>
+                    <input type="checkbox" name="checkboxx" class="form-check-input">
                     <label class="form-check-label">I agree to the Privacy Policy</label>
                 </div>
-                <button type="submit" class="btn btn-primary w-100">Sign Up</button>
+                <button type="submit" name="submit" class="btn btn-primary w-100">Sign Up</button>
             </form>
         </div>
         <div class="image-container"></div>
     </div>
 </body>
+
 </html>
