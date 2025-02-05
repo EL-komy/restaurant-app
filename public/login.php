4de28bd74d586155f3492809b09d2fa769b2b68e
EL-komy <elkomy.mohamed30@gmail.com>
2025-02-05 19:51:32 +0200

    design of loginPage

--- public/login.php
+++ public/login.php
@@ -1,1 +1,68 @@
 <?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: rgb(242, 233, 233);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .container {
+            display: flex;
+            width: 800px;
+            background: white;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+        .form-container {
+            width: 50%;
+            padding: 10px;
+        }
+        .image-container {
+            width: 50%;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            background: url('images/ai-generative-3d-style-design-of-fried-chicken-in-yellow-background-photo.jpg') center/cover;
+        }
+        .btn-primary {
+            background-color: rgb(159, 39, 39);
+            border-color: rgb(159, 39, 39);
+        }
+        .btn-primary:hover {
+            background-color: darkred;
+            border-color: darkred;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="form-container">
+            <h2 class="text-center">Login</h2>
+            <form action="" method="POST" enctype="multipart/form-data">
+                
+                <div class="mb-3">
+                    <label class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Password</label>
+                    <input type="password" name="passwordd" class="form-control" required>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Login</button>
+            </form>
+        </div>
+        <div class="image-container"></div>
+    </div>
+</body>
+</html>
