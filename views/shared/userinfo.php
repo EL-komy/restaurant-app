6d3469cebae496d37a0288acd25213a2f8d67ef7
zienab3li <zienab2152002@gmail.com>
2025-02-09 21:42:47 +0200

    userProfile

--- views/shared/userinfo.php
+++ views/shared/userinfo.php
@@ -118,8 +118,11 @@ if($email){
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 83959cf (showing user info)
+=======
+>>>>>>> d4ec197 (userProfile)
         <div><strong>Name:</strong> <?=$user['user_name']?></div>
         <div><strong>Email:</strong> <?=$user['email']?></div>
         <div><strong>Address:</strong> <?=$user['addresss']?></div>
@@ -129,13 +132,19 @@ if($email){
         <div><strong>Address:</strong> <?=$user['address']?></div>
 >>>>>>> 418de30 (showing user info)
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d4ec197 (userProfile)
 =======
         <div><strong>Name:</strong> <?=$user['user_name']?></div>
         <div><strong>Email:</strong> <?=$user['email']?></div>
         <div><strong>Address:</strong> <?=$user['addresss']?></div>
 >>>>>>> 25d0ded (userProfile)
+<<<<<<< HEAD
 =======
 >>>>>>> 83959cf (showing user info)
+=======
+>>>>>>> d4ec197 (userProfile)
         <div><strong>Phone:</strong> <?=$user['phone']?></div>
     </div>
     
