b019f88b4dd79992a97ac47724bd193679d5e472
EL-komy <elkomy.mohamed30@gmail.com>
2025-02-11 15:45:58 +0200

    update on eidtpage

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 class Database {
     private $host = "localhost";
     private $db_name = "fryco";
-    private $username = "root";
-    private $password = "Zainab@215";
+    private $username = "mk";
+    private $password = "221999@mk";
+    private $port = "3306"; 
     public $conn;
 
     public function connect() {
