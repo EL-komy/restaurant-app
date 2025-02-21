771bd56a563b8526b3ec9a49f1245d3573d90eff
omar571mamdouh <ok1119076@gmail.com>
2025-02-21 18:32:18 +0200

    reservation ul and php

--- config/db.php
+++ config/db.php
@@ -3,13 +3,14 @@ class Database {
     private $host = "localhost";
     private $db_name = "fryco";
     private $username = "root";
-    private $password = "Zainab@215";
+    private $password = "";
+    private $port = "3307"; 
     public $conn;
 
     public function connect() {
         $this->conn = null;
         try {
-            $dsn = "mysql:host=" . $this->host .  ";dbname=" . $this->db_name;
+            $dsn = "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
