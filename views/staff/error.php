9b35c130732ec1e1ceb78e6d1f8f5b9e5670ff50
zienab3li <zienab2152002@gmail.com>
2025-02-24 17:15:34 +0200

    error.php

--- views/staff/error.php
+++ views/staff/error.php
@@ -6,8 +6,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        html, body {
+            height: 100%;
+            width: 100%;
+        }
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover; /* Ensures the image covers the whole page without distortion */
+        }
+    </style>
 </head>
 <body>
-    you 're not allowed to view this page'
+    <img src="error.jpeg" alt="error image">
 </body>
 </html>
